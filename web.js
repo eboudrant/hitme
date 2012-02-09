878869715e0e038baba7fd98d24bae5e6482e86b
Emmanuel Boudrant <emmanuel.boudrant@gmail.com>
2012-02-09 10:33:19 -0800

    set a fixed timeout

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Cache-control': 'no-store'
+    });
+    //var randomTimeout = Math.floor(Math.random() * 500);
+    var randomTimeout = 100;
+    //console.log('accepting (' + randomTimeout + 'msec)...');
+    setTimeout(function() {
+        response.end('Hello World! (' + randomTimeout + 'msec)');
+    }, randomTimeout);
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 7000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
